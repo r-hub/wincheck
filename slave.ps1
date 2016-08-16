3ba975750d2cd5072b51ffc092d80f21ec28a877
Gabor Csardi <csardi.gabor@gmail.com>
2016-08-16 09:44:21 -0700

    Minimal check script, no deps yet

--- slave.ps1
+++ slave.ps1
@@ -1,2 +1,28 @@
 
-& 'C:\Program Files\R\R-devel\bin\R' CMD check $args
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True,Position=1)]
+    [string]$URL
+)
+
+$R = 'C:\Program Files\R\R-devel\bin\R'
+
+# --------------------------------------------------------------------
+Write-Verbose ( "Downloading " + $URL )
+
+
+$filename = $URL.Substring($URL.LastIndexOf("/") + 1)
+
+cd $home
+
+Invoke-WebRequest -Uri $URL -OutFile $filename
+
+# --------------------------------------------------------------------
+Write-Verbose ( "Extracting " + $filename )
+
+tar xzf $filename
+
+# --------------------------------------------------------------------
+Write-Verbose ( "Checking " + $filename )
+
+$R CMD check $filename
